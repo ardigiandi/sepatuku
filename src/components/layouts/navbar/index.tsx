df134460ca467f28efcfd54bf951186d003b37fc
ardigiandi <ardiansyahg9080@gmail.com>
2024-08-10 17:04:05 +0700

    add : fix

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     const { data } = useSession()
     return (
         <div className="flex justify-between items-center px-10 py-5 border-b border-black">
-            <h1 className="text-2xl font-medium">Sepatuku</h1>
+            <h1 className="text-2xl font-semibold">Sepatuku</h1>
             <button className="bg-black text-white px-4 py-1" onClick={() => (data ? signOut() : signIn())}>{data ? "Logout" : "Login"}</button>
         </div>
     )
